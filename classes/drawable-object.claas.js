d805f69e27934dbb648a249b776bdd12b805abb4
Christoph Haase <christophhaase2@gmx.de>
2023-04-28 13:28:19 +0200

    update

--- classes/drawable-object.claas.js
+++ classes/drawable-object.claas.js
@@ -31,4 +31,30 @@ class DrawableObject {
 
     }
 
+
+
+
+
+
+    // draw Frames
+    drawFrame(ctx) {
+        if (this instanceof JellyFish) {
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
+    }
+
+    drawFrameCharacter(ctx) {
+        if (this instanceof Character) {
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x + 40, this.y + 80, this.width - 80, this.height - 120);
+            ctx.stroke();
+        }
+    }
+    // <--------Ende-------------->
 }
