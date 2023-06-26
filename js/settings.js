c8fde4eb99b93b1ec0ab66a401b41dd78f944b54
Christoph Haase <christophhaase2@gmx.de>
2023-06-26 20:08:46 +0200

    update images

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,8 @@
+let volume;
+let currentVolume;
+let Image1 = true;
+
+
 function openSettings() {
     document.getElementById('settings-main-overlay').classList.remove('d-none');
 }
@@ -9,13 +14,31 @@ function closeSettings() {
 
 
 document.addEventListener("DOMContentLoaded", function () {
+    const soundImage = document.getElementById('sound-icon')
+
+    soundImage.addEventListener('click', function () {
+        if (Image1) {
+            soundImage.src = 'img/assests/sound_off.png';
+            currentVolume = volume;
+            volume = 0;
+            Image1 = false;
+        } else {
+            soundImage.src = 'img/assests/sound_on.png';
+            volume = currentVolume;
+            Image1 = true;
+        }
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    volume = 50;
     let volumeSlider = document.getElementById("volume-slider");
     let volumeValue = document.getElementById("volume-value");
 
     volumeSlider.addEventListener("input", function () {
-        var volume = volumeSlider.value;
+        volume = Number(volumeSlider.value);
         volumeValue.innerHTML = volume;
     });
-
-
 });
